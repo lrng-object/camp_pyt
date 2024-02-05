7df625e515972aa60ba10fe6efbe33c1e95fb1cb
lrng-object <152777550+lrng-object@users.noreply.github.com>
2024-02-05 15:02:40 +0100

    Add files via upload
    Añadido el ejercicio 7, algunos comentarios para numerar los ejercicios.

--- m2c3python.py
+++ m2c3python.py
@@ -1,25 +1,38 @@
+#1)
 some_string = "I'm a string"
 some_number = 13
 some_boolean = True
 some_list = [1,2,3,4,5] 
 
+#2)
 first_three_letters = some_string[ : 3]
 print(first_three_letters)
 
+#3)
 first_element = some_list[0]
 print(first_element)
 
+#4)
 new_number = some_number + 10
 print(new_number)
 
+#5)
 last_element = some_list[-1]
 print(format(last_element))
 
+#6)
 names = 'harry,alex,susie,jared,gail,conner'
 names_list = names.split(',')
 print(names_list)
 
+#7)
+first_word = some_string[ : some_string.index(" ")].upper()
+new_string =  first_word + some_string[some_string.index(" ") : ]
+print(new_string)
+
+#8)
 print(f"Total: {some_number}")
 print("Total: {0}".format(some_number))
 
+#9)
 print('"hello world"')
